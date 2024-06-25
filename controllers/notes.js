5b1edd7e4386746a05c2de749448364cec4b2eb5
parisa-naeim-ga <parisa.naeim@gmail.com>
2024-06-25 13:15:17 +1000

    add delete button

--- controllers/notes.js
+++ controllers/notes.js
@@ -43,3 +43,23 @@ router.put("/:id", async (req, res) => {
     res.redirect(`/users/${req.session.user._id}/notes`);
   }
 });
+
+// delete
+console.log("first log");
+router.delete("/:id", async (req, res) => {
+  try {
+    console.log("second log;jdg");
+    const user = await User.findById(req.session.user._id);
+    const index = user.notes.findIndex((item) => item._id == req.params.id);
+    console.log(index);
+    // user.notes[index].title = req.body.title;
+    // user.notes[index].content = req.body.content;
+    console.log(user);
+    user.notes.splice(index, 0);
+    await user.save();
+    res.redirect(`/users/${req.session.user._id}/notes`);
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
